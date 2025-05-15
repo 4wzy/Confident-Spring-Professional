9e6070a3cd78c6584a6882c2b6b77c328c2e5819
4wzy <opkoala4@gmail.com>
2025-05-15 13:17:28 +0100

    Module #2

--- src/main/java/com/justingrosu/myfancypdfinvoices/service/InvoiceService.java
+++ src/main/java/com/justingrosu/myfancypdfinvoices/service/InvoiceService.java
@@ -1,35 +1,52 @@
 package com.justingrosu.myfancypdfinvoices.service;
 
-import com.justingrosu.myfancypdfinvoices.context.Application;
 import com.justingrosu.myfancypdfinvoices.model.Invoice;
 import com.justingrosu.myfancypdfinvoices.model.User;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@Component
 public class InvoiceService {
 
+    private final List<Invoice> invoices = new CopyOnWriteArrayList<>();
+
     private final UserService userService;
+    private final String cdnUrl;
 
-    List<Invoice> invoices = new CopyOnWriteArrayList<>();
+    @PostConstruct
+    public void init() {
+        System.out.println("Fetching PDF Template from S3...");
+    }
 
-    public InvoiceService(UserService userService) {
+    @PreDestroy
+    public void shutdown() {
+        System.out.println("Deleting downloaded templates...");
+    }
+
+    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
+        this.cdnUrl = cdnUrl;
     }
 
     public List<Invoice> findAll() {
         return invoices;
     }
 
-    public Invoice create(String userID, Integer amount) {
-        User user = userService.findById(userID);
+    public Invoice create(String userId, Integer amount) {
+        User user = userService.findById(userId);
         if (user == null) {
-            throw new IllegalStateException("User not found");
+            throw new IllegalStateException();
         }
 
-        Invoice invoice = new Invoice(userID, amount, "http://www.africau.edu/images/default/sample.pdf");
+
+        // TODO real pdf creation and storing it on network server
+        Invoice invoice = new Invoice(userId, amount, cdnUrl + "/images/default/sample.pdf");
         invoices.add(invoice);
         return invoice;
     }
-
 }
--- src/main/java/com/justingrosu/myfancypdfinvoices/service/UserService.java
+++ src/main/java/com/justingrosu/myfancypdfinvoices/service/UserService.java
@@ -1,9 +1,11 @@
 package com.justingrosu.myfancypdfinvoices.service;
 
 import com.justingrosu.myfancypdfinvoices.model.User;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
+@Component
 public class UserService {
     public User findById(String id) {
         String randomName = UUID.randomUUID().toString();
